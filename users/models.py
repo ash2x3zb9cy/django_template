79aebcad28169552ea412d999e65370b245ead67
Ash Brent-Carpenter <ash@sent.com>
2019-11-28 18:51:07 +0000

    add SiteUser model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class SiteUser(models.Model):
+	"""Wraps a User model to facilitate extra fields.
+
+	Example usage: profile pictures, user bios, etc.
+
+	Access the Django User object using the equiv_user field.
+	"""
+	equiv_user = models.OneToOneField(
+		settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE,
+		verbose_name="related internal user object",
+		related_name="site_user",
+	)

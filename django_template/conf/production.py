6b11047aa15dab59703fe5d6981bc7505f0d7062
Ash Brent-Carpenter <ash@sent.com>
2019-11-28 20:45:20 +0000

    add some production security settings

--- django_template/conf/production.py
+++ django_template/conf/production.py
@@ -4,5 +4,20 @@ try:
 	SECRET_KEY = os.environ['SECRET_KEY']
 except KeyError as e:
 	raise EnvironmentError('SECRET_KEY environment variable needs to be set in production', e)
+
 # just in case
 Debug = False
+
+# configure this with the domain you are hosting on.
+ALLOWED_HOSTS = []
+
+# Read the docs before enabling this.
+# SECURE_HSTS_SECONDS = ??????
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+
+# Need SSL!
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
